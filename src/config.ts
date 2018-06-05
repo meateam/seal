d5b1779cca0cc004628e35c72b3827426e164266
shahar-y <shya95@gmail.com>
2018-06-05 15:17:37 +0300

    Code review 1

--- src/config.ts
+++ src/config.ts
@@ -1,42 +1,40 @@
 type Config = {
-    port: number,
-    db: {
-        host: string
-        port: string
-        name: string
-    }
-}
+  port: number,
+  db: {
+    host: string,
+    port: string,
+    name: string,
+  },
+};
 
 const dev: Config = {
-    port: 3000,
-    db: {
-        host: 'localhost',
-        port: '27017',
-        name: ''
-    }
-}
-
+  port: 3000,
+  db: {
+    host: 'localhost',
+    port: '27017',
+    name: '',
+  },
+};
 
 // Change to Production Enviorment
 const prod: Config = {
-    port: 3000,
-    db: {
-        host: 'localhost',
-        port: '27017',
-        name: 'prodDB'
-
-    }
-}
+  port: 3000,
+  db: {
+    host: 'localhost',
+    port: '27017',
+    name: 'prodDB',
+  },
+};
 
 function getConfig(type: string) {
-    switch(type) {
-        case 'dev':
-            return dev;
-        case 'prod':
-            return prod;
-        default:
-            return dev;
-    }    
+  switch (type) {
+    case 'dev':
+      return dev;
+    case 'prod':
+      return prod;
+    default:
+      return dev;
+  }
 }
 
 export const config = getConfig(process.env.NODE_ENV || 'dev');
