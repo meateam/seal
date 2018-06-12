cc6abce2edaab14b486293986e856616d980a7ed
shahar-y <shya95@gmail.com>
2018-06-12 13:51:02 +0300

    removed index.ts, fixed route

--- src/helper/routing.ts
+++ src/helper/routing.ts
@@ -1,9 +1,9 @@
 import { routeEnum } from './ENUMS';
 import { userRouter } from '../user/user.router';
-const api = "/api";
+const api = '/api';
 export function initRouting(app) {
 
-  app.use(api + routeEnum.USERS, userRouter);
+  app.use(api + routeEnum.USER, userRouter);
 
   app.get('/', (req, res) => {
     res.send('Main page of the application');
