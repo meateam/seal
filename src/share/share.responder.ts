9229e7bda5fa18d355db75a07c200bfbcca9ecaa
AllKinds <yonatald@gmail.com>
2018-06-23 22:22:32 +0300

    chore: Added very basic router and responder(which doesn't work...)

---
+++ src/share/share.responder.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+
+export class ShareResponder {
+  static async get(req: Request, res: Response) {
+    res.json({});
+  }
+  static async getByID(req: Request, res: Response) {
+    res.json({});
+  }
+  static async create(req: Request, res: Response) {
+    res.json({});
+  }
+  static async update(req: Request, res: Response) {
+    res.json({});
+  }
+  static async delete(req: Request, res: Response) {
+    res.json({});
+  }
+}
