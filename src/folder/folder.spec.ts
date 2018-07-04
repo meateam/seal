fd5793d478da1bb8b59c9a1a9e3097f5007e12da
shahar-y <shya95@gmail.com>
2018-07-04 12:59:57 +0300

    Continued TDD on folder #23

--- src/folder/folder.spec.ts
+++ src/folder/folder.spec.ts
@@ -1,14 +1,28 @@
 import { IFolder } from './folder.interface';
 import * as chai from 'chai';
+import { config } from '../config';
 import * as mongoose from 'mongoose';
 import { createFolders } from '../helpers/functions';
+import { folderModel } from './folder.model';
 
 const expect = chai.expect;
 const TOTAL_FOLDERS: number = 4;
-let testFolders : IFolder[];
+const testFolders: IFolder[] = createFolders(TOTAL_FOLDERS);
 
 describe('Test Folder', () => {
   before(() => {
-    testFolders = createFolders(TOTAL_FOLDERS);
+    (<any>mongoose).Promise = global.Promise;
+    mongoose.connect(`mongodb://${config.db.host}:${config.db.port}/${config.db.name}`);
+  });
+  beforeEach(async () => {
+    folderModel.remove({}, (err) => { });
+    folderModel.collection.insert(testFolders, (err, docs) => {
+      if (err) {
+        console.error(err);
+        // throw new serverError
+      } else {
+        console.log('Multiple documents inserted to Collection');
+      }
+    });
   });
 });
