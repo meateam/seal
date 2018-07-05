f6ebf56b987536c2495e5819c68e6487fa107b62
shahar-y <shya95@gmail.com>
2018-07-05 11:39:39 +0300

    Added -add- and -deleteById- generic tests #24

--- src/specs/spec.controller.ts
+++ src/specs/spec.controller.ts
@@ -5,9 +5,10 @@ import { config } from '../config';
 import { EntityTypes } from '../helpers/enums';
 import { EntityConfig, user_entity } from './entity_config';
 import { UserController } from '../user/user.controller';
+import { ServerError } from '../errors/application';
 
 const expect: Chai.ExpectStatic = chai.expect;
-const TEST_ITEMS = 4;
+const TOTAL_ITEMS = 4;
 const ctr = new UserController();
 runTests(ctr);
 function runTests(controller: UserController) {
@@ -15,7 +16,7 @@ function runTests(controller: UserController) {
   describe(`Test type ${controller.controllerType}`, () => {
 
     before(() => {
-      testItems = controller.createItems(TEST_ITEMS);
+      testItems = controller.createItems(TOTAL_ITEMS);
       (<any>mongoose).Promise = global.Promise;
       mongoose.connect(`mongodb://${config.db.host}:${config.db.port}/${config.db.name}`);
     });
@@ -33,10 +34,36 @@ function runTests(controller: UserController) {
     });
 
     describe('#getAll', () => {
-      it(`should return a collection with ${TEST_ITEMS} items`, async () => {
+      it(`should return a collection with ${TOTAL_ITEMS} items`, async () => {
         const itemsReturned = await controller.getAll();
         expect(itemsReturned).to.not.be.empty;
         expect(itemsReturned).to.have.lengthOf(testItems.length);
+      });
+    });
+
+    describe('#add', () => {
+      it('should add a new item to the collection', async () => {
+        const user = controller.createItems(1)[0];
+        await controller.add(user);
+        const usersReturned = await controller.getAll();
+        expect(usersReturned).to.not.be.empty;
+        expect(usersReturned).to.have.lengthOf(testItems.length + 1);
+      });
+      it('should throw exception when trying to add new item with existed id', async () => {
+        try {
+          await controller.add(testItems[0]);
+          expect(false).to.be.true;
+        } catch (err) {
+          expect(err).to.be.instanceof(ServerError);
+        }
+      });
+    });
+
+    describe('#deleteById', () => {
+      it('should delete a single user', async () => {
+        await controller.deleteById(testItems[0]._id);
+        const usersReturned = await controller.getAll();
+        expect(usersReturned).to.have.lengthOf(TOTAL_ITEMS - 1);
       });
     });
 
