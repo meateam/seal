21a858f77b222022fe9a39a402df00fba752aa82
tal2301 <tnahari2301@gmail.com>
2018-07-31 16:04:28 +0300

    refactor add responder

---
+++ src/file/file.responder.ts
@@ -0,0 +1,87 @@
+import * as express from 'express';
+import { IFile } from './file.interface';
+import { fileController } from './file.controller';
+import { fileModel } from './file.model';
+import { upload } from './storage/storage.manager';
+
+export class FileResponder {
+
+  static async create(req: express.Request, res: express.Response) {
+    if (!req.files) {
+      res.status(400).send({ message: 'Files cannot be empty' });
+    } else {
+      const files = (<Express.Multer.File[]>req.files).map((val) => {
+        const file: IFile = new fileModel({
+          fileName: val.originalname,
+          fileSize: val.size,
+          path: val.path,
+          fileType: val.originalname,
+          creationDate: Date.now(),
+          modifyDate: null,
+          Owner: 'User',
+          Parent: 'rootFolder',
+        });
+        return file;
+      });
+
+      try {
+        const ret = await fileController.create(files);
+        return res.send({ message: 'File saved successfully' });
+      } catch (err) {
+        console.log(err.message);
+        return res.status(500).send({ message: 'Could not save file - ' + err.message });
+      }
+    }
+  }
+
+  static async getAll(req: express.Request, res: express.Response) {
+    try {
+      const ret = await fileController.getFiles();
+      return res.json({ success: true, return: ret });
+    } catch (err) {
+      return res.status(500).send({ message: 'Could not retrieve files - ' + err.message });
+    }
+  }
+
+  static async delete(req: express.Request, res: express.Response) {
+    try {
+      const ret = await fileController.delete(req.params.id);
+      return res.send({ message: 'File deleted successfully' });
+    } catch (err) {
+      return res.status(500).send({ message: 'Could not delete file - ' + err.message });
+    }
+  }
+
+  static async get(req: express.Request, res: express.Response) {
+    try {
+      let ret;
+      if (req.query.fromDate || req.query.toDate) {
+        ret = await fileController.findByDate(req.query.fromDate,
+                                              req.query.toDate);
+      } else if (req.query.fieldType) {
+        ret = await fileController.getFiles(req.query.fieldType,
+                                            req.params.fieldValue);
+      } else {
+        ret = await fileController.findById(req.params.fieldValue);
+      }
+      if (ret) {
+        return res.json({ success: true, return: ret });
+      }
+      return res.status(404).send({ message: 'No files found' });
+    } catch (err) {
+      return res.status(500).send({
+        message: 'Could not retrieve files - ' + err.message });
+    }
+  }
+
+  static async update(req: express.Request, res: express.Response) {
+    try {
+      const file: Partial<IFile> = req.body;
+      const ret = await fileController.update(req.params.id, file);
+      return res.send({ message: 'File updated successfully' });
+    } catch (err) {
+      console.log(err.message);
+      return res.status(500).send({ message: 'Could not update file - ' + err.message });
+    }
+  }
+}
--- src/file/file.router.ts
+++ src/file/file.router.ts
@@ -3,85 +3,13 @@ import { IFile } from './file.interface';
 import { fileController } from './file.controller';
 import { fileModel } from './file.model';
 import { upload } from './storage/storage.manager';
+import warpAsync from '../helpers/warpAsync';
+import { FileResponder } from './file.responder';
 
 export let fileRouter: express.Router = express.Router();
 
-fileRouter.post('/upload', upload, async (req: express.Request, res: express.Response) => {
-  if (!req.files) {
-    res.status(400).send({ message: 'Files cannot be empty' });
-  } else {
-    const files = (<Express.Multer.File[]>req.files).map((val) => {
-      const file: IFile = new fileModel({
-        fileName: val.originalname,
-        fileSize: val.size,
-        path: val.path,
-        fileType: val.originalname,
-        creationDate: Date.now(),
-        modifyDate: null,
-        Owner: 'User',
-        Parent: 'rootFolder',
-      });
-      return file;
-    });
-
-    try {
-      const ret = await fileController.create(files);
-      return res.send({ message: 'File saved successfully' });
-    } catch (err) {
-      console.log(err.message);
-      return res.status(500).send({ message: 'Could not save file - ' + err.message });
-    }
-  }
-});
-
-fileRouter.get('/', async (req: express.Request, res: express.Response) => {
-  try {
-    const ret = await fileController.getFiles();
-    return res.json({ success: true, return: ret });
-  } catch (err) {
-    return res.status(500).send({ message: 'Could not retrieve files - ' + err.message });
-  }
-});
-
-fileRouter.delete('/:id', async (req: express.Request, res: express.Response) => {
-  try {
-    const ret = await fileController.delete(req.params.id);
-    return res.send({ message: 'File deleted successfully' });
-  } catch (err) {
-    return res.status(500).send({ message: 'Could not delete file - ' + err.message });
-  }
-});
-
-fileRouter.get('/:fieldValue',
-               async (req: express.Request, res: express.Response) => {
-                 try {
-                   let ret;
-                   if (req.query.fromDate || req.query.toDate) {
-                     ret = await fileController.findByDate(req.query.fromDate,
-                                                           req.query.toDate);
-                   } else if (req.query.fieldType) {
-                     ret = await fileController.getFiles(req.query.fieldType,
-                                                         req.params.fieldValue);
-                   } else {
-                     ret = await fileController.findById(req.params.fieldValue);
-                   }
-                   if (ret) {
-                     return res.json({ success: true, return: ret });
-                   }
-                   return res.status(404).send({ message: 'No files found' });
-                 } catch (err) {
-                   return res.status(500).send({
-                     message: 'Could not retrieve files - ' + err.message });
-                 }
-               });
-
-fileRouter.put('/:id' , async (req: express.Request, res: express.Response) => {
-  try {
-    const file: Partial<IFile> = req.body;
-    const ret = await fileController.update(req.params.id, file);
-    return res.send({ message: 'File updated successfully' });
-  } catch (err) {
-    console.log(err.message);
-    return res.status(500).send({ message: 'Could not update file - ' + err.message });
-  }
-});
+fileRouter.post('/upload', upload, warpAsync(FileResponder.create));
+fileRouter.get('/', warpAsync(FileResponder.getAll));
+fileRouter.delete('/:id', warpAsync(FileResponder.delete));
+fileRouter.get('/:fieldValue', warpAsync(FileResponder.get));
+fileRouter.put('/:id' , warpAsync(FileResponder.update));
