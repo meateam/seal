451e6e2f4e54c8f776a5cac932f2fcecda31eb6f
shahar-y <shya95@gmail.com>
2018-08-01 10:29:35 +0300

    Merge

--- src/user/user.validaotr.ts
+++
@@ -1,9 +0,0 @@
-import Validator from '../validator';
-import { UserController } from './user.controller';
-import { doesntThrowAsync } from '../helpers/functions';
-
-export default class UserValidator extends Validator {
-  async userExists(id): Promise<boolean> {
-    return doesntThrowAsync(UserController.getById, [id]);
-  }
-}
