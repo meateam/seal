c3f6b33a8a6bf5bf0fbccad836a0e84a381538a5
shahar-y <shya95@gmail.com>
2018-08-05 19:49:10 +0300

    refactor: UserModel

--- src/helpers/functions.ts
+++ src/helpers/functions.ts
@@ -2,7 +2,7 @@
  * Helper functions for the tests
  */
 import { IUser } from '../user/user.interface';
-import { userModel } from '../user/user.model';
+import { UserModel } from '../user/user.model';
 import { folderModel } from '../folder/folder.model';
 import { IFolder } from '../folder/folder.interface';
 
@@ -27,7 +27,7 @@ export function createUsers(numUsers: number): IUser[] {
   const rand2: string = Math.random().toString(36).substring(2, 7);
   const testUsers: IUser[] = [];
   for (let i: number = 0; i < numUsers; i++) {
-    const user: IUser = new userModel({
+    const user: IUser = new UserModel({
       uniqueID: rand2 + '_' + (numUsers * 10 + i),
       hierarchy: 'Aman/Sapir/MadorHaim/' + i,
       name: 'User' + i,
