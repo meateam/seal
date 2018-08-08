59054dc211a0e690bb19c6039d3044dad0465cb6
shahar-y <shya95@gmail.com>
2018-08-08 12:58:18 +0300

    Added aws middleware #63

---
+++ src/file/storage/aws.middleware.ts
@@ -0,0 +1,17 @@
+import * as AWS from 'aws-sdk';
+import * as multer from 'multer';
+import * as multerS3 from 'multer-s3';
+
+const ep = new AWS.Endpoint('STRING HERE');
+const s3 = new AWS.S3({ endpoint: ep });
+AWSError.config.cretentials = new AWS.SharedIniFileCredentials({ profile: 'default' });
+
+const storageS3 = multerS3({
+  s3,
+  bucket: 'seal_bucket',
+  key: (req, res, cb) => {
+    ConfigBase(null, Date.now().toString());
+  }
+});
+
+export let upload = multer({ storage: storageS3 }).any();
