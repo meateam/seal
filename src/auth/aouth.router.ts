7337510e146285408120a339473e0061fb2ec2e8
shahar-y <shya95@gmail.com>
2018-09-06 10:13:41 +0300

    installing auth imports

--- src/auth/aouth.router.ts
+++
@@ -1,7 +0,0 @@
-import * as express from 'express';
-import * as path from 'path';
-export let authRouter: express.Router = express.Router();
-
-authRouter.get('/metadata.xml', (req, res) => {
-  res.sendFile(path.join(__dirname, 'metadata.xml'));
-});
