da5ff0d19e22f38b137a91af88cfc7697b4a93e4
shahar-y <shya95@gmail.com>
2018-10-28 16:59:32 +0200

    fix ip

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 export const environment = {
   production: false,
-  server: 'http://40.115.124.214:9000/',
-  api: 'http://40.115.124.214:9000/api'
+  server: 'http://40.113.72.159:9000/',
+  api: 'http://40.113.72.159:9000/api'
 };
 
 /*
