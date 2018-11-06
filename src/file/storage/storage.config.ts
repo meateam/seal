6e89c5b09a4d02434a42c77abd433e5a56c016ac
shahar-y <shya95@gmail.com>
2018-11-06 14:39:21 +0200

    trying in new minio server 2

--- src/file/storage/storage.config.ts
+++ src/file/storage/storage.config.ts
@@ -1,7 +1,7 @@
 import { config } from '../../config';
 
 export const storageURL : string = 'https://23.102.42.153:9000/';
-export const storageIP  : string = '40.113.65.101';
-export const bucketName : string = `${config.storage}`;
-export const accessKey  : string = 'sealminio';
-export const secretKey  : string = '/QixUY2oU6YY0f+LSjbVsmlFxfUqSn5LA7x26nSVUDKJcU7kn9kxNRY+wdLNDHBWfxnOdR8SO1XmQ2A5NRr2Uw==';
+export const storageIP  : string = '23.102.42.153';
+export const bucketName : string = `test`;
+export const accessKey  : string = '8UP9T6JILUUNMYH36PLD';
+export const secretKey  : string = 'QRKlYLrM2aQaPsmxxeNib7bW9con1KM6enYaNIYY';
